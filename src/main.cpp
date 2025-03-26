1b8e6d5415b0aea0b05781e4035fdd5dcd5c0e6a
Hiroshi Bowman <hcbowman@gmail.com>
2025-03-26 15:23:40 +0000

    Added image tiles and image select button

--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,17 @@ const int gameWidth = 300;
 const int gameHeight = 300;
 const int tileWidth = 100;
 const int tileHeight = 100;
+const int buttonWidth = 350;
+const int buttonHeight = 108;
 
 const int tilesPerRow = gameWidth / tileWidth;
 const int gamePositionX = (screenWidth - gameWidth) / 2;
-const int gamePositionY = (screenHeight - gameHeight) / 2;
+const int gamePositionY = ((screenHeight - buttonHeight) - gameHeight) / 2;
+const int buttonPositionX = (screenWidth - buttonWidth) / 2;
+const int buttonPositionY = (screenHeight - buttonHeight + gameHeight) / 2;
 
 int LocateMouse(void) {
-  int tileNumber = 1;  // Change the one to a zero
+  int tileNumber = 0;
   int mouseX = GetMouseX() - gamePositionX;
   int mouseY = GetMouseY() - gamePositionY;
 
@@ -40,16 +44,21 @@ int LocateMouse(void) {
 
 int main(void)
 {
+  bool stateDisplayImage = true;
+  InitWindow(screenWidth, screenHeight, "Lesson Two");
 
-
-  InitWindow(screenWidth, screenHeight, "Lesson One");
-
-  std::vector<Texture2D> tiles;
+  std::vector<Texture2D> numberedTiles;
+  std::vector<Texture2D> imageTiles;
   for (int tilenumber = 0; tilenumber < 9; tilenumber++) {
     char tilepath[32];
     std::sprintf(tilepath, "resources/Tile_%d.png", tilenumber);
-    tiles.push_back(LoadTexture(tilepath));
+    numberedTiles.push_back(LoadTexture(tilepath));
+    std::sprintf(tilepath, "resources/Image_%d.png", tilenumber);
+    imageTiles.push_back(LoadTexture(tilepath));
   }
+  std::vector<Texture2D> imageButton;
+  imageButton.push_back(LoadTexture("resources/unpressed.png"));
+  imageButton.push_back(LoadTexture("resources/pressed.png"));
 
   SetTargetFPS(15);
   while (!WindowShouldClose())
@@ -59,6 +68,17 @@ int main(void)
     if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT))
     {
       selectedTile = LocateMouse();
+      if (selectedTile == 0) {
+        int mouseX = GetMouseX();
+        int mouseY = GetMouseY();
+
+        if ((mouseY > buttonPositionY) &&
+        (mouseY < buttonPositionY + buttonHeight) &&
+        (mouseX > buttonPositionX) &&
+        (mouseX < buttonPositionX + buttonWidth)) {
+          stateDisplayImage = !stateDisplayImage;
+        }
+      }
     }
 
     BeginDrawing();
@@ -70,7 +90,8 @@ int main(void)
                          gameWidth,
                          gameHeight,
                          BLACK);
-      for (int tilecounter = 0; static_cast<size_t>(tilecounter) < tiles.size(); tilecounter++)
+      std::vector<Texture2D> chosenTiles = stateDisplayImage ? imageTiles : numberedTiles; 
+      for (int tilecounter = 0; static_cast<size_t>(tilecounter) < chosenTiles.size(); tilecounter++)
       {
         int rowNumber = tilecounter / tilesPerRow;
         int colNumber = tilecounter % tilesPerRow;
@@ -78,19 +99,35 @@ int main(void)
         int y = gamePositionY + (rowNumber * tileHeight);
         if (tilecounter == selectedTile) 
         {
-          DrawTexture(tiles.at(tilecounter), x, y, PINK);
+          DrawTexture(chosenTiles.at(tilecounter), x, y, PINK);
         } else {
-          DrawTexture(tiles.at(tilecounter), x, y, WHITE);
+          DrawTexture(chosenTiles.at(tilecounter), x, y, WHITE);
         }
+      }
+
+
+      if (stateDisplayImage) {
+        DrawTexture(imageButton[0], buttonPositionX, buttonPositionY, WHITE);
+      } else {
+        DrawTexture(imageButton[1], buttonPositionX, buttonPositionY, WHITE);
       }
 
     }
     EndDrawing();
   }
 
-  for (Texture2D tile: tiles) 
+  for (Texture2D tile: numberedTiles) 
   {
     UnloadTexture(tile);
+  }
+
+  for (Texture2D tile: imageTiles)
+  {
+    UnloadTexture(tile);
+  }
+
+  for (Texture2D button:imageButton) {
+    UnloadTexture(button);
   }
 
   CloseWindow();
